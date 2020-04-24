8e88d9becd74d7e58b7a2ca4f66dd3eed11d199e
Jake Hamilton <jakehamtexas@gmail.com>
2020-04-23 19:54:51 -0500

    Adds and styles slider in place of dropdown.

--- assets/vue/components/Form.vue
+++ assets/vue/components/Form.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <label for="text">Text</label>
-    <input id="text" type="text" v-model="localText" />
-    <label for="size">Size</label>
-    <select id="size" v-model="localSize">
-      <option v-for="i of 10" :key="i" :value="i">{{ i }}</option>
-    </select>
+    <div class="iaas__form-field">
+      <label for="text">Text</label>
+      <input id="text" type="text" v-model="localText" />
+    </div>
+    <div class="iaas__form-field">
+      <label for="size">Size</label>
+      <span class="size-label">{{ localSize }}</span>
+      <input type="range" min="1" max="10" v-model="localSize" class="slider" />
+    </div>
   </div>
 </template>
 <script>
@@ -40,6 +43,56 @@ export default {
 };
 </script>
 <style lang="scss">
+.iaas {
+  &__form-field {
+    display: flex;
+    align-content: space-between;
+    label {
+      padding-top: 10px;
+    }
+    input,
+    select {
+      margin-left: 15px;
+    }
+    .size-label {
+      margin-left: 15px;
+    }
+    .slider {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 100%;
+      height: 25px;
+      background: #d3d3d3;
+      outline: none;
+      opacity: 0.7;
+      -webkit-transition: 0.2s;
+      transition: opacity 0.2s;
+      margin-top: 10px;
+    }
+    .slider::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 25px;
+      height: 25px;
+      background: #4caf50;
+      cursor: pointer;
+      margin-bottom: 10px;
+    }
+
+    .slider::-moz-range-thumb {
+      width: 25px;
+      height: 25px;
+      background: #4caf50;
+      cursor: pointer;
+      margin-bottom: 10px;
+    }
+
+    .size-label,
+    .slider {
+      padding-top: 10px;
+    }
+  }
+}
 input[type="text"],
 select {
   -webkit-appearance: none;
